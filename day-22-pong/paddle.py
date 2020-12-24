264ee0050405499c2a6d836fa7510d6a77ec30eb
leba0495 <61469904+leba0495@users.noreply.github.com>
2020-12-24 14:09:42 -0500

    Add files via upload

---
+++ day-22-pong/paddle.py
@@ -0,0 +1,21 @@
+from turtle import Turtle
+
+
+# Create paddle
+class Paddle(Turtle):
+    def __init__(self, position):
+        super().__init__()
+        self.shape("square")
+        self.color("white")
+        self.penup()
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.goto(position)
+
+    # Move paddle
+    def move_up(self):
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
+
+    def move_down(self):
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y)
