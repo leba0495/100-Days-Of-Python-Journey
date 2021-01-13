78c170ac9b6da6ba740b4b319038169f39631d38
leba0495 <61469904+leba0495@users.noreply.github.com>
2021-01-13 18:05:07 -0500

    Update main.py

--- day-32-birthday-wisher/main.py
+++ day-32-birthday-wisher/main.py
@@ -3,22 +3,30 @@ import datetime as dt
 import pandas
 import random
 
+# Make the csv a DataFrame in order to access it more easily
 birthday_file = pandas.read_csv("birthdays.csv")
 
+#Acess today's information (day, month, year..)
 the_day_of = dt.datetime.now()
 the_day_of_birthday = (the_day_of.month, the_day_of.day)
 
+#Make dictionary's key to searchable by (month, day)
 birthdays_dict = {(data_row.month, data_row.day): data_row for (index, data_row) in birthday_file.iterrows()}
+
+# To pick a random letter
 letter_numb = random.randint(1, 3)
 
-my_email = "lifevicebyluis@gmail.com"
-my_password = "Brito_LV_arias25"
+my_email = "your email"
+my_password = "your password"
 
+# Checks for birthday person
 if the_day_of_birthday in birthdays_dict:
     birthday_person = birthdays_dict[the_day_of_birthday]
+    
     with open(f"./letter_templates/letter_{letter_numb}.txt", mode="r") as rand_letter:
         chosen_letter = rand_letter.read()
         chosen_letter = chosen_letter.replace("[NAME]", birthday_person['name'])
+        
     with smtplib.SMTP("smtp.gmail.com") as connection:
         connection.starttls()
         connection.login(user=my_email, password=my_password)
